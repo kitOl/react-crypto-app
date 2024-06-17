248d176fb6d43de82520c78078e0d407fb31cf58
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-06-17 21:51:43 +0300

    add react form

--- src/components/layout/AppHeader.jsx
+++ src/components/layout/AppHeader.jsx
@@ -81,8 +81,9 @@ export default function AppHeader() {
         title="Add Asset"
         onClose={() => setDrawer(false)}
         open={drawer}
+        destroyOnClose
       >
-        <AddAssetForm />
+        <AddAssetForm onClose={() => setDrawer(false)} />
       </Drawer>
     </Layout.Header>
   );
